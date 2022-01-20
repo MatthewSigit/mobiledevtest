a2dc3a4d8dd2ff36239c78eb4535e5dd9f0a5c4a
Matthew Sigit <message4mas@gmail.com>
2022-01-20 21:32:30 +0700

    Initial Commit

---
+++ Sleep App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sleep App
+//
+//  Created by Matthew Sigit on 20/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sleep App/Sleep_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_AppApp.swift
+//  Sleep App
+//
+//  Created by Matthew Sigit on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
